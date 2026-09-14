1d18c25f77c12ff4564f0274e63e9ccd568381b0
Saif Nasr <saifnasr418@gmail.com>
2026-09-15 02:55:20 +0300

    Part A

--- CSharpBasicsAssignment/Program.cs
+++ CSharpBasicsAssignment/Program.cs
@@ -1,1 +1,19 @@
-﻿Console.WriteLine("Hello, World!");
+﻿//.csproj -> File which contains the config and settings of the project(ProjectFile)
+//Program.cs -> File where you write the code
+// /bin & /obj -> Folders Created When the solution build 
+// This project uses .slnx format
+// Advantage Of .sln foramt -> Performance -> .sln is optimized for fast loading of large solutions
+
+
+namespace CSharpBasicsAssignment; // It removes the level of indentation to reduce nesting and improve readability(make the file easier to scan)
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("hello world");
+    }
+
+
+}
+
+
